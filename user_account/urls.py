0528cc6e08a076e4281e61cb04f7624ac041ce53
Robert <rcoffie22@yahoo.com>
2021-08-30 14:54:35 +0000

    second, 30-08-2021

--- user_account/urls.py
+++ user_account/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
    path('',views.Registration,name="register"),
    path('login/',views.Login,name='login'),
    path('logout/',views.UserLogout,name='logout'),
+   path('profile_edit/',views.EditProfile,name='profile_edit'),
 ]
--- user_account/views.py
+++ user_account/views.py
@@ -44,3 +44,23 @@ def Login(request):
 def UserLogout(request):
   logout(request)
   return redirect('blog_post:home')
+
+
+def EditProfile(request):
+  if request.method == 'POST':
+    u_form = UserUpdateForm(request,POST,instance=request.user)
+    p_form = ProfileUpdateForm(reqest.POST, instance=request.user.profile)
+    if u_form.is_valid() and p_form.is_valid():
+      u_form.save()
+      p_form.save()
+      return redirect('author_dashboard:home')
+  else:
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+  
+  context = {
+    'u_form':u_form,
+    'p_form':p_form
+  }
+  
+  return render(request,'user_account/profile_edit.html',context)
